a139dedf831992aede8d1465f05fe87a60a69a13
BernieRebolledo <bl.rebolledo@gmail.com>
2016-01-09 14:09:42 -0500

    subiendo a heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  ENV["TW_ID"] = "jCmNOAfPylWz4JGGqhmaVxbPC"
+  ENV["TW_SECRET"] = "xYNwCh83lS0GRs05EvWE0G5Kk578J35G3VVgYr27VNLZ88v141"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
